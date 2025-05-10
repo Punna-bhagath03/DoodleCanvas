fd2b83d1add470d6f3e496bc087321e1b4013059
Punna-bhagath03 <punnabhagath03@gmail.com>
2025-05-10 12:56:12 +0530

    Added zod and jwt schemaa

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,16 +1,41 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from './config';
+import { JWT_SECRET } from '@repo/backend-common/config';
 import middleware from './middleware';
-
+import {
+  CreateRoomSchema,
+  CreateUserSchema,
+  SigninSchema,
+} from '@repo/common/types';
 const app = express();
 
 app.post('/signup', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const data = CreateUserSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({
+      message: 'incorrect input',
+    });
+    return;
+  }
+
+  //db call
+  res.json({
+    userId: '123',
+  });
 });
 
 app.post('/signin', (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({
+      message: 'incorrect input',
+    });
+    return;
+  }
+
+  //
   const userId = 1;
   const token = jwt.sign(
     {
@@ -23,5 +48,18 @@ app.post('/signin', (req, res) => {
   });
 });
 
-app.post('/room', middleware, (req, res) => {});
+app.post('/room', middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({
+      message: 'incorrect input',
+    });
+    return;
+  }
+
+  res.json({
+    roomId: '123',
+  });
+});
 app.listen(3001);
